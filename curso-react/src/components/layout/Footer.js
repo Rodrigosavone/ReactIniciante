da3e28fbcebd3b5ab00a42b43bbcb268edac6469
Rodrigosavone <33437232+Rodrigosavone@users.noreply.github.com>
2022-09-08 11:40:59 -0300

    Router

---
+++ curso-react/src/components/layout/Footer.js
@@ -0,0 +1,26 @@
+import { FaFacebook, FaInstagram, FaTwitter } from 'react-icons/fa'
+import styles from './Footer.module.css'
+
+function Footer() {
+
+  return (
+    <>
+      <footer>
+        <ul className={styles.social_list}>
+          <li>
+            <FaFacebook />
+          </li>
+          <li>
+            <FaInstagram />
+          </li>
+          <li>
+            <FaTwitter />
+          </li>
+        </ul>
+        <p>Nosso rodapé</p>
+      </footer>
+    </>
+  )
+}
+
+export default Footer
